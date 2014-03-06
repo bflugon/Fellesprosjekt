8134e4969198350cb8111710d698d51758dc74b0
Markus Lund <markus@teknologia.no>
2014-03-06 11:21:10 +0100

    La til førsteutkast av kalendervisning

--- Fellesprosjekt/src/main/Controller.java
+++ Fellesprosjekt/src/main/Controller.java
@@ -1,4 +1,15 @@
 package main;
 
-public class Controller {
+public class Controller implements Initializable {
+
+    public ListView<String> listViewMonday;
+    ObservableList<String> test1;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        test1 = FXCollections.observableArrayList("Single", "Double", "Suite", "Family App", "Double", "Suite", "Family App", "Double", "Suite", "Family App", "Double", "Suite", "Family App", "Double", "Suite", "Family App", "Double", "Suite", "Family App", "Double", "Suite", "Family App", "Double", "Suite", "Family App");
+        System.out.println(listViewMonday);
+        listViewMonday.setItems(test1);
+        listViewMonday.setCellFactory(ComboBoxListCell.forListView(test1));
+    }
 }
