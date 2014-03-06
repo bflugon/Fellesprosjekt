52a8dc53bf89b8c6ecedd76aa2fd06cf7f48f0d3
bflugon <bflugon@gmail.com>
2014-03-06 10:58:02 +0100

    Fikset filer

--- Fellesprosjekt/src/main/Main.java
+++ Fellesprosjekt/src/main/Main.java
@@ -1,19 +1,23 @@
 package main;
 
-/**
- * Created with IntelliJ IDEA.
- * User: FF63
- * Date: 2/27/14
- * Time: 4:29 PM
- * To change this template use File | Settings | File Templates.
- */
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    /**
-     * Entry point
-     * @param args
-     */
-    public static void main(String[] args){
+public class Main extends Application {
 
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
