2a0f9374982dbbcdd3a1deaa6c62763c2bdf11fd
Markus Lund <markus@teknologia.no>
2014-03-06 13:41:16 +0100

    Utkast av calendar og meeting fxml-filene er påbegynt. Samt litt logikk for sammenkobling i calendarController. Også organisering i mapper.

--- Fellesprosjekt/src/main/calendar/CalendarController.java
+++ Fellesprosjekt/src/main/calendar/CalendarController.java
@@ -1,15 +1,20 @@
-package main;
+package main.calendar;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.cell.ComboBoxListCell;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class CalendarController implements Initializable{
 
     public ListView<String> listViewMonday;
     ObservableList<String> test1;
@@ -20,5 +25,20 @@ public class Controller implements Initializable {
         System.out.println(listViewMonday);
         listViewMonday.setItems(test1);
         listViewMonday.setCellFactory(ComboBoxListCell.forListView(test1));
+    }
+
+    public void makeMeeting(ActionEvent actionEvent) throws Exception{
+
+
+
+        Stage newStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("../meeting/meeting.fxml"));
+        newStage.setTitle("Hello World");
+        newStage.setScene(new Scene(root));
+        newStage.show();
+
+
+
+
     }
 }
