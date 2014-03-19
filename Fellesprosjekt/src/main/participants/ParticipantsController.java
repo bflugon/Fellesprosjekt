8e78bfaf6a8697f9e69878d3b6a892bb56fd2b8c
Markus Lund <markus@teknologia.no>
2014-03-19 17:47:44 +0100

    Participants straks ferdig, mangler å fjerne en bug som oppstår når man lager nytt møte

--- Fellesprosjekt/src/main/participants/ParticipantsController.java
+++ Fellesprosjekt/src/main/participants/ParticipantsController.java
@@ -2,19 +2,19 @@ package main.participants;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import main.Register;
 import main.RegisterSingleton;
+import main.meeting.MeetingController;
 import model.Appointment;
 import model.Group;
-import model.MeetingRoom;
 import model.Person;
 import util.GuiUtils;
-import javafx.event.ActionEvent;
-import javafx.scene.Node;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -51,31 +51,45 @@ public class ParticipantsController implements Initializable{
 
     private Appointment appointment;
 
+    public void setParent(MeetingController parent) {
+        this.parent = parent;
+    }
+
+    private MeetingController parent;
 
 
-    public void participantsOkOnAction(ActionEvent actionEvent) {
+
+    public void confirmParticipantsButtonOnAction(ActionEvent actionEvent) {
+        parent.setPeopleToInvite(new ArrayList<Person>((invitedPeopleTableView.getItems())));
         GuiUtils.closeWindow(actionEvent);
     }
 
-    public void confirmParticipantsButtonOnAction(ActionEvent actionEvent) {
-    }
-
     public void addSelectedParticipantButtonOnAction(ActionEvent actionEvent) {
-        ObservableList<Person> currentInvitedPeople = invitedPeopleTableView.getItems();
-        ObservableList<Person> currentAllPeople = allPersonsTableView.getItems();
-        currentInvitedPeople.add(selectedPerson);
-        currentAllPeople.remove(selectedPerson);
-        invitedPeopleTableView.setItems(currentInvitedPeople);
-        allPersonsTableView.setItems(currentAllPeople);
+        if (selectedPerson!=null){
+            ObservableList<Person> currentInvitedPeople = invitedPeopleTableView.getItems();
+            ObservableList<Person> currentAllPeople = allPersonsTableView.getItems();
+            currentInvitedPeople.add(selectedPerson);
+            currentAllPeople.remove(selectedPerson);
+            invitedPeopleTableView.setItems(currentInvitedPeople);
+            allPersonsTableView.setItems(currentAllPeople);
+            selectedPerson = null;
+            selectedGroup = null;
+            personToBeRemoved = null;
+        }
     }
 
     public void removeSelectedParticipantButton(ActionEvent actionEvent) {
-        ObservableList<Person> currentList = listToBeRemovedFrom.getItems();
-        ObservableList<Person> currentAllPeople = allPersonsTableView.getItems();
-        currentList.remove(personToBeRemoved);
-        currentAllPeople.add(personToBeRemoved);
-        listToBeRemovedFrom.setItems(currentList);
-        allPersonsTableView.setItems(currentAllPeople);
+        if (personToBeRemoved != null){
+            ObservableList<Person> currentList = listToBeRemovedFrom.getItems();
+            ObservableList<Person> currentAllPeople = allPersonsTableView.getItems();
+            currentList.remove(personToBeRemoved);
+            currentAllPeople.add(personToBeRemoved);
+            listToBeRemovedFrom.setItems(currentList);
+            allPersonsTableView.setItems(currentAllPeople);
+            personToBeRemoved = null;
+            selectedPerson = null;
+            selectedGroup = null;
+        }
 
     }
 
@@ -85,7 +99,7 @@ public class ParticipantsController implements Initializable{
 
             if (!((TableView)(e.getSource())).getSelectionModel().getSelectedCells().isEmpty()){
                 Person p = (Person)((TableView)(e.getSource())).getSelectionModel().getSelectedItem();
-                System.out.println("Selected person: " + p);
+//                System.out.println("Selected person: " + p);
                 selectedPerson = p;
                 selectedGroup = null;
                 personToBeRemoved = null;
@@ -96,7 +110,7 @@ public class ParticipantsController implements Initializable{
 
             if (!((TableView)(e.getSource())).getSelectionModel().getSelectedCells().isEmpty()){
                 Group g = (Group)((TableView)(e.getSource())).getSelectionModel().getSelectedItem();
-                System.out.println("Selected group: " + g);
+//                System.out.println("Selected group: " + g);
                 selectedPerson = null;
                 selectedGroup = g;
                 personToBeRemoved = null;
@@ -111,7 +125,7 @@ public class ParticipantsController implements Initializable{
                 selectedPerson = null;
                 personToBeRemoved = p;
                 listToBeRemovedFrom = ((TableView)(e.getSource()));
-                System.out.println("Person to be removed: " + p);
+//                System.out.println("Person to be removed: " + p);
             }
 
         }else if(e.getSource() == attendingPeopleTableView){
@@ -122,7 +136,7 @@ public class ParticipantsController implements Initializable{
                 selectedPerson = null;
                 personToBeRemoved = p;
                 listToBeRemovedFrom = ((TableView)(e.getSource()));
-                System.out.println("Person to be removed: " + p);
+//                System.out.println("Person to be removed: " + p);
             }
 
         }else if(e.getSource() == notAttendingPeopleTableView){
@@ -133,31 +147,30 @@ public class ParticipantsController implements Initializable{
                 selectedPerson = null;
                 personToBeRemoved = p;
                 listToBeRemovedFrom = ((TableView)(e.getSource()));
-                System.out.println("Person to be removed: " + p);
+//                System.out.println("Person to be removed: " + p);
             }
         }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        //Legger til alle personer, treng for å lage nytt møte
         allPersons = RegisterSingleton.sharedInstance().getRegister().getPersons();
-        allGroups = RegisterSingleton.sharedInstance().getRegister().getGroups();
-
         ObservableList<Person> personOL = FXCollections.observableArrayList(allPersons);
         allPersonsTableColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("Name"));
         allPersonsTableView.setItems(personOL);
 
+        //Legger til alel grupper
+        allGroups = RegisterSingleton.sharedInstance().getRegister().getGroups();
         ObservableList<Group> groupOL = FXCollections.observableArrayList(allGroups);
         allGroupsTableColumns.setCellValueFactory(new PropertyValueFactory<Group, String>("GroupName"));
         allGroupsTableView.setItems(groupOL);
 
-
-        updateGroupTable();
+//        updateGroupTable();
     }
 
     public void updateTables() {
-        if(!(appointment.getAppointmentName() == null)){
+        if(appointment.getAppointmentName() != null){
             updateAttendingTable();
             updateNotAttendingTable();
             updateInvitedTable();
@@ -177,10 +190,10 @@ public class ParticipantsController implements Initializable{
         attendingPeople = RegisterSingleton.sharedInstance().getRegister().getAttendingPeople(appointment.getAppointmentID());
 
         if (attendingPeople != null){
-            System.out.println("alle deltagere:");
-            for (Person p : attendingPeople){
-                System.out.println(p);
-            }
+//            System.out.println("alle deltagere:");
+//            for (Person p : attendingPeople){
+//                System.out.println(p);
+//            }
 
             ObservableList<Person> attendingPersonOL = FXCollections.observableArrayList(attendingPeople);
             attendingPeopleTableColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("Name"));
@@ -191,28 +204,30 @@ public class ParticipantsController implements Initializable{
     }
 
     private void updateInvitedTable() {
+        attendingPeople = RegisterSingleton.sharedInstance().getRegister().getAttendingPeople(appointment.getAppointmentID());
         ArrayList<Person> allInvitedPeople = RegisterSingleton.sharedInstance().getRegister().getInvitees(appointment.getAppointmentID());
         invitedPeople = new ArrayList<>();
-        for (Person ap : attendingPeople){
-            for (Person ip : allInvitedPeople){
-                if(!ap.getUsername().equals(ip.getUsername())){
-                    invitedPeople.add(ip);
+        if (attendingPeople != null  && allInvitedPeople!=null){
+            for (Person ap : attendingPeople){
+                for (Person ip : allInvitedPeople){
+                    if(!ap.getUsername().equals(ip.getUsername())){
+                        invitedPeople.add(ip);
+                    }
                 }
             }
+
         }
 
-
         if(invitedPeople != null){
-            System.out.println("alle inviterte:");
-            for (Person p : invitedPeople){
-                System.out.println(p);
-            }
 
             ObservableList<Person> personOL = FXCollections.observableArrayList(invitedPeople);
             invitedPeopleTableColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("Name"));
             invitedPeopleTableView.setItems(personOL);
+
         }else{
+
             System.out.println("No invited people");
+
         }
     }
 
@@ -223,16 +238,31 @@ public class ParticipantsController implements Initializable{
     }
 
     private void updatePersonTable() {
-
+        ArrayList<Person> allInvitedPeople = RegisterSingleton.sharedInstance().getRegister().getInvitees(appointment.getAppointmentID());
+        if (allInvitedPeople == null){
+            allInvitedPeople = new ArrayList<>();
+        }
+        allPersons = RegisterSingleton.sharedInstance().getRegister().getPersons();
         ArrayList<Person> notInvited = new ArrayList<Person>();
-        for (Person p : allPersons){
-            if(!invitedPeople.contains(p)){
-                notInvited.add(p);
+        for (Person ap : allPersons){
+            if(!notInvited.contains(ap)){
+                notInvited.add(ap);
+                System.out.println("Added dude: " + ap);
             }
+            for (Person ip : allInvitedPeople){
+                if ((ap.getUsername().equals(ip.getUsername()))){
+                    notInvited.remove(ap);
+                }
+            }
+        }
+        System.out.println("Alle personer som ikke er inviterte!:");
+        for (Person p : notInvited){
+            System.out.println(p);
         }
 
         ObservableList<Person> personOL = FXCollections.observableArrayList(notInvited);
         allPersonsTableColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("Name"));
+        System.out.println("SETTER PERSONEN: " + personOL);
         allPersonsTableView.setItems(personOL);
     }
 
